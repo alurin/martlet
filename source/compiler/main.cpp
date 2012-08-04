25b00ed768c1d349b5ea19073f4d3d933fab1069
Vasiliy Sheredeko <piphon@gmail.com>
2012-08-04 20:29:31 +0600

    Version information and file arguments of compiler

--- source/compiler/main.cpp
+++ source/compiler/main.cpp
@@ -19,15 +19,17 @@ using namespace apus::lang;
 
 int main (int argc, const char **argv)
 {
-    if (argc == 0) {
+    std::cout << APUS_NAME << " " << APUS_VERSION_STRING << " ©" << APUS_COPYRIGHT_YEARS << "\n";
+    if (argc < 2) {
         std::cerr << "Run with filename\n";
         return -1;
     } else {
-        for (int i = 0; i < argc; ++i) {
+        for (int i = 1; i < argc; ++i) {
             try {
                 Driver* driver = Driver::createFromFile(argv[i]);
                 driver->parse();
                 delete driver;
+                std::cout << "Parse '" << argv[i] << " is complete\n";
             } catch (const Exception& ex) {
                 std::cerr << ex;
             }
